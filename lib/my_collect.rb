c1ebdc83ee9b34133ee930db6a2dde2250d55c82
gajen <meetgajen@yahoo.com>
2021-02-28 11:18:09 -0500

    commit tree e207525927b6c8514eab44625aa0ca9718b2ae01

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+languages= ['ruby', 'javascript', 'python', 'objective-c'] 
+ students= ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] 
+
+def my_collect(array)
+    empty_array = []
+    count=0
+    while count<array.length 
+    empty_array<<yield(array[count])
+        count+=1
+ 
+    end
+    empty_array
+end
 
 
+my_collect(languages) do |language|
+  return language.upcase
+  end
+     
+  my_collect(students) do |student|
+   return student.split(" ").first
+  end
